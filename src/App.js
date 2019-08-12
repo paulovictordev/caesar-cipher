1b5067e9d6fc7a898fa5335c4e2b5a1f3c9386bd
machap <paulinhodj10@gmail.com>
2019-08-11 21:15:06 -0300

    Criando pagina principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Main from './pages/Main';
+
 function App() {
   return (
-    <h1>App</h1>
+    <Main />
   );
 }
 
---
+++ src/pages/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Main.css';
+
+export default function Main() {
+    return (
+        <h1>Teste exibindo MAin</h1>
+    );
+}
