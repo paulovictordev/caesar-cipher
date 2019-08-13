a7515e56ac9d2ceac2a1bae934c3013f6b574b5d
machap <paulinhodj10@gmail.com>
2019-08-12 21:31:01 -0300

    Criando funcoes para utilizar no componente

---
+++ src/assets/crypto.js
@@ -0,0 +1,60 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e',
+        'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o',
+        'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y',
+        'z'];
+
+export function encrypt(phrase, key) {
+    let phraseEncrypt = [];
+    let phraseSize = 0;
+
+    phrase.forEach(symbol => {
+        alphabet.forEach(symbolAlphabet => {
+            if (symbol === symbolAlphabet) {
+                if ((alphabet.lastIndexOf(symbolAlphabet) + key) < (alphabet.length - 1)) {
+                    phraseEncrypt.push(alphabet[alphabet.lastIndexOf(symbolAlphabet) + key]);
+                } else {
+                    phraseEncrypt.push(alphabet[(alphabet.lastIndexOf(symbolAlphabet) + key) - alphabet.length]);
+                }
+            }
+        });
+
+        phraseSize++;
+
+        if (phraseEncrypt.length !== phraseSize) {
+            phraseEncrypt.push(symbol);
+        }
+    });
+
+    return phraseEncrypt.join('');
+}
+
+export function decrypt(phrase, key) {
+    let phraseDecrypt = [];
+    let phraseSize = 0;
+
+    phrase.forEach(symbol => {
+        alphabet.forEach(symbolAlphabet => {
+            if (symbol === symbolAlphabet) {
+                if ((alphabet.lastIndexOf(symbolAlphabet) - key) >= 0) {
+                    phraseDecrypt.push(alphabet[alphabet.lastIndexOf(symbolAlphabet) - key]);
+                } else {
+                    phraseDecrypt.push(alphabet[(alphabet.lastIndexOf(symbolAlphabet) - key) + alphabet.length]);
+                }
+            }
+        });
+
+        phraseSize++;
+
+        if (phraseDecrypt.length !== phraseSize) {
+            phraseDecrypt.push(symbol);
+        }
+    });
+
+    return phraseDecrypt.join('');
+};
+
+export function phraseToArray(phrase) {
+    return phrase.toLowerCase().split('');
+}
