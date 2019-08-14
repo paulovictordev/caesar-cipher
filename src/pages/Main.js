a08d378a91ee93967971b6bc0b6e099731f2d14f
machap <paulinhodj10@gmail.com>
2019-08-13 23:01:03 -0300

    Alterando componente para utilizar state

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 import { encrypt, decrypt, phraseToArray } from '../assets/crypto';
 
 export default function Main() {
+    const [key, setKey] = useState(3);
+    const [textDecipher, setTextDecipher] = useState('');
+    const [textCipher, setTextCipher] = useState('');
+
     function cipher(e) {
         e.preventDefault();
 
-        let key = parseInt(document.getElementById('key').value);
-        let textDecipher = document.getElementById('textDecipher').value;
-        const pharse = phraseToArray(textDecipher);
-        const textCipher = encrypt(pharse, key);
-        document.getElementById('textCipher').value = textCipher;
+        let pharse = phraseToArray(textDecipher);
+        let textCipher = encrypt(pharse, key);
+        setTextCipher(textCipher);
     }
 
     function decipher(e) {
         e.preventDefault();
 
-        let key = parseInt(document.getElementById('key').value);
-        let textCipher = document.getElementById('textCipher').value;
-        const pharse = phraseToArray(textCipher);
-        const textDecipher = decrypt(pharse, key);
-        document.getElementById('textDecipher').value = textDecipher;
+        let pharse = phraseToArray(textCipher);
+        let textDecipher = decrypt(pharse, key);
+        setTextDecipher(textDecipher);
     }
 
     return (
@@ -30,19 +30,26 @@ export default function Main() {
                     id="textDecipher" 
                     rows="10" 
                     cols="40"
+                    value={ textDecipher }
+                    onChange={ e => setTextDecipher(e.target.value)}
                 ></textarea>
-                    <div className="button-container">
-                        <button onClick={cipher}>Cipher</button>
-                        <input 
-                            id="key"
-                            placeholder="Key"
-                        ></input>
-                        <button onClick={decipher}>Decipher</button>
-                    </div>
+                <div className="button-container">
+                    <button onClick={cipher}>Cipher</button>
+                    <input 
+                        id="key"
+                        type="number"
+                        placeholder="Key"
+                        value={key}
+                        onChange={ e => setKey(e.target.value)}
+                    ></input>
+                    <button onClick={decipher}>Decipher</button>
+                </div>
                 <textarea 
                     id="textCipher" 
                     rows="10" 
                     cols="40"
+                    value= { textCipher }
+                    onChange={ e => setTextCipher(e.target.value) }
                 ></textarea>
             </div>
         </div>        
