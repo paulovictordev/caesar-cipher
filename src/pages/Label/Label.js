f97580dd2b79463f2d747bb941bb84155805d93c
machap <paulinhodj10@gmail.com>
2019-08-16 23:55:19 -0300

    Criando componente para exibir descricao

---
+++ src/pages/Label/Label.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Label.css';
+
+export default function Label() {
+    return (
+        <div className="label-container">
+            <p>
+                ... Caesar Cipher is a type of substitution cipher in which each letter of the text is replaced by others by a fixed number of times.
+            </p>
+        </div>
+        
+    );
+}
