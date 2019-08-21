8d1e6f12aad46c79d0bc0c9b127a7d72d4060125
machap <paulinhodj10@gmail.com>
2019-08-20 22:08:33 -0300

    Alterando texto de exibiçao

--- src/pages/Label/Label.js
+++ src/pages/Label/Label.js
@@ -5,8 +5,7 @@ export default function Label() {
     return (
         <div className="label-container">
             <p>
-                ... Caesar Cipher is a type of substitution cipher in which each letter of the text is replaced by others by a fixed number of times.
-            </p>
+                Caesar Cipher </p>
         </div>
         
     );
