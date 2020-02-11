ab1634647c2359cf2203f81b1ac8bbb276ebacde
Paulo Victor <paulinhodj10@gmail.com>
2020-02-10 22:13:09 -0300

    Alterando Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,12 +5,11 @@ import Logo from '../../assets/img/github-logo.png'
 
 export default function Footer() {
     return (
-        <footer>
-            <p>Create by &nbsp; <i>Paulo Victor</i>
-                <a href="https://github.com/paulovictordev">
-                    <img className="logo" src={Logo} alt="GitHub" />
-                </a>   
-            </p>         
+        <footer className="footer">
+            <span>Create by &nbsp; <i>Paulo Victor</i></span>
+            <a href="https://github.com/paulovictordev">
+                <img className="logo" src={Logo} alt="GitHub" />
+            </a>
         </footer>
     );
 }
